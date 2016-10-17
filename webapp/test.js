abdf41fd9eccd0e9d0b19dad2cdebd110b8bd0ca
Christophe Lestienne <christophe.lestienne@umicore.com>
2016-10-17 11:47:39 +0000

    push

---
+++ webapp/test.js
@@ -0,0 +1,18 @@
+sap.ui.define([], function() {
+	"use strict";
+
+	QUnit.module("Module name", {
+
+		beforeEach: function() {
+
+		},
+
+		afterEach: function() {
+
+		}
+	});
+
+	QUnit.test("Should Test description", function(assert) {
+		assert.ok(false, "Implement me");
+	});
+});
